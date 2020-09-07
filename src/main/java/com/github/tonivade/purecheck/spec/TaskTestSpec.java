d86916ab31490ccf2f3744414a97905ae8075128
Antonio Muñoz <antoniogmc@gmail.com>
2020-09-07 08:29:33 +0200

    adds TaskTestSpec

---
+++ src/main/java/com/github/tonivade/purecheck/spec/TaskTestSpec.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2020, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
+package com.github.tonivade.purecheck.spec;
+
+import static com.github.tonivade.purefun.concurrent.FutureOf.toFuture;
+import static com.github.tonivade.purefun.effect.TaskOf.toTask;
+import java.util.concurrent.Executor;
+import com.github.tonivade.purecheck.TestCase;
+import com.github.tonivade.purecheck.TestFactory;
+import com.github.tonivade.purecheck.TestReport;
+import com.github.tonivade.purecheck.TestSuite;
+import com.github.tonivade.purefun.concurrent.Future;
+import com.github.tonivade.purefun.data.NonEmptyList;
+import com.github.tonivade.purefun.effect.Task_;
+import com.github.tonivade.purefun.instances.FutureInstances;
+import com.github.tonivade.purefun.instances.TaskInstances;
+
+/**
+ * A silly class to define the test case factory and reuse
+ *
+ * @author tonivade
+ */
+public abstract class TaskTestSpec {
+
+  protected final TestFactory<Task_> it = TestFactory.factory(TaskInstances.monadDefer());
+  
+  @SafeVarargs
+  public static <E> TestSuite<Task_, E> suite(
+      String name, TestCase<Task_, E, ?> test, TestCase<Task_, E, ?>... tests) {
+    return new TestSuite<Task_, E>(TaskInstances.monad(), name, NonEmptyList.of(test, tests)) {
+      @Override
+      public TestReport<E> run() {
+        return runK().fix(toTask()).safeRunSync().get();
+      }
+      
+      @Override
+      public Future<TestReport<E>> parRun(Executor executor) {
+        return runK().fix(toTask()).foldMap(FutureInstances.monadDefer()).fix(toFuture());
+      }
+    };
+  }
+
+}
